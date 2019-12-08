e55cf4bbbab3d93aa69bc917684ecadb386acd37
TuitaKing <735367737@qq.com>
2019-12-08 15:53:58 +0800

    first commit

---
+++ src/main/java/com/milacat/distributed/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.milacat.distributed.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String test () {
+        return "Hello Springboot";
+    }
+}
