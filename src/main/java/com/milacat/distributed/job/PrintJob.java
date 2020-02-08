6b4103326abe887e3b94ce0ff068ae68bf417301
milacat <xingmei_v1@163.com>
2020-02-08 16:18:53 +0800

    接入xxl-job

---
+++ src/main/java/com/milacat/distributed/job/PrintJob.java
@@ -0,0 +1,19 @@
+package com.milacat.distributed.job;
+
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import com.xxl.job.core.log.XxlJobLogger;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PrintJob {
+
+    @XxlJob("demoJobHandler")
+    public ReturnT<String> demoJobHandler(String param) throws Exception {
+        XxlJobLogger.log("XXL-JOB, Hello World.");
+        System.out.println("XXL-JOB, Hello World.");
+        return ReturnT.SUCCESS;
+    }
+
+
+}
